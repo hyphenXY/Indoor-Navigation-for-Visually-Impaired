a62140795f75aca924deb0f03213e5df63a286fd
Shivam Yadav <shivam20126@iiitd.ac.in>
2024-03-20 23:58:52 +0530

    first

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "Hello, World!"
+
+
+@app.route('/json', methods=['POST'])
+def android_data():
+    data = request.json  # Assuming data is sent as JSON
+    print(data)
+    return "Data received"
+
+
+if __name__ == '__main__':
+    app.run(host="192.168.47.20", debug=True, port=5000)
